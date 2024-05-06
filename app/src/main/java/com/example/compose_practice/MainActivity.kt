eaf2428b5145fd6101be25c35944a092c0cebddc
김승우 <rnqhqaltjs@naver.com>
2024-05-07 02:35:25 +0900

    Initial Commit

---
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.example.compose_practice
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.compose_practice.ui.theme.Compose_practiceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Compose_practiceTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier,
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ButtonExample (onButtonClicked = {
+                        Toast.makeText(this,  "Send clicked.", Toast.LENGTH_SHORT).show()
+                    })
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(color = Color.Red,
+        text = "Hello $name!\nHello $name!\nHello $name!",
+        modifier = Modifier.size(300.dp)   ,
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = FontFamily.Cursive,
+        letterSpacing = 2.sp,
+        maxLines = 2,
+        textDecoration = TextDecoration.Underline,
+        textAlign = TextAlign.End
+    )
+}
+
+@Composable
+fun ButtonExample(onButtonClicked: () -> Unit) {
+    Button(
+        onClick = onButtonClicked,
+        enabled = true,
+        border = BorderStroke(10.dp, Color.Magenta),
+        shape = CircleShape,
+        contentPadding = PaddingValues(20.dp)
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Send,
+           contentDescription = null
+        )
+        Spacer(
+            modifier = Modifier.size(ButtonDefaults.IconSpacing)
+        )
+        Text(text = "Send")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Compose_practiceTheme {
+        ButtonExample (onButtonClicked = {
+        })
+    }
+}
+
