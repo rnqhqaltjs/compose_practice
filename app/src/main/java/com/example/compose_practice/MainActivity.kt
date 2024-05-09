96babd68eda47fcc262a3641f9129737a5c8fdb7
김승우 <rnqhqaltjs@naver.com>
2024-05-10 02:39:06 +0900

    checkbox practice

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
@@ -32,11 +33,16 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -61,15 +67,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Compose_practiceTheme {
                 // A surface container using the 'background' color from the theme
-                Surface (
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Column {
-                        CardEx(cardData)
-                        CardEx(cardData)
-                    }
-                }
+                CheckBoxEX()
             }
         }
     }
@@ -319,7 +317,8 @@ fun CardEx(cardData: CardData) {
                 contentScale = ContentScale.Crop,
                 contentDescription = cardData.imageDescription,
                 placeholder = ColorPainter(placeHolderColor),
-                modifier = Modifier.size(32.dp)
+                modifier = Modifier
+                    .size(32.dp)
                     .clip(CircleShape)
             )
             Spacer(modifier = Modifier.size(8.dp))
@@ -343,13 +342,32 @@ data class CardData(
     val description: String
 )
 
+@Composable
+fun CheckBoxEX() {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+//        var checked = remember { mutableStateOf(false) }
+
+//        var checked by remember { mutableStateOf(false) }
+
+        val (getter, setter) = remember { mutableStateOf(false) }
+        Checkbox(
+            checked = getter,
+            onCheckedChange = setter
+        )
+        Text(
+            text = "프로그래머입니까?",
+            modifier = Modifier.clickable {
+                setter(!getter)
+            }
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Compose_practiceTheme {
-        Row {
-            CardEx(MainActivity.cardData)
-        }
+        CheckBoxEX()
     }
 }
 
